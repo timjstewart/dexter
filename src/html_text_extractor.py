d1513d9eb68eceb92bbf73b730e8801e60193a5f
Tim Stewart <timothy.stewart@pearson.com>
2013-11-18 16:09:13 -0700

    formatting

--- src/html_text_extractor.py
+++ src/html_text_extractor.py
@@ -7,18 +7,22 @@ class HtmlTextExtractor(TextExtractor):
         super(HtmlTextExtractor, self).__init__("HTML", file_name)
         self._soup = None
         self._text = text
+
     @staticmethod
     def can_analyze(file_name):
         ext = TextExtractor.get_extension(file_name) 
         return ext.lower() in [ '.html', '.htm' ]
+
     def get_title(self):
         title = self._get_soup().title
         if title:
             return (title.string or u'').strip()
         else:
             return unicode('')
+
     def get_full_text(self):
         return self._get_soup().get_text().strip()
+
     def _get_soup(self):
         if not self._soup:
             self._soup = BeautifulSoup(self._text)
--- src/text_extractor.py
+++ src/text_extractor.py
@@ -4,10 +4,13 @@ class TextExtractor(object):
     def __init__(self, name, file_name = None):
         self.name = name
         self.file_name = file_name
+
     def get_title(self):
         pass
+
     def get_full_text(self):
         pass
+
     @staticmethod
     def get_extension(file_name):
         return os.path.splitext(file_name)[1]
